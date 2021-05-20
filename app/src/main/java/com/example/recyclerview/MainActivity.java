84dccc148b53861117b4c37816927fbf974955da
ritamonteiro <ritaalvesmonteiro@gmail.com>
2021-05-20 20:06:15 -0300

    remove RecyclerItem.java

--- app/src/main/java/com/example/recyclerview/MainActivity.java
+++ app/src/main/java/com/example/recyclerview/MainActivity.java
@@ -1,15 +1,12 @@
 package com.example.recyclerview;
 
+import android.os.Bundle;
+import android.widget.LinearLayout;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
-import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.LinearLayout;
-import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -28,28 +25,6 @@ public class MainActivity extends AppCompatActivity {
         createMovies();
         Adapter adapter = new Adapter(movieList);
         setupRecyclerView(adapter);
-
-        mainRecyclerView.addOnItemTouchListener(new RecyclerItemClickListener(getApplicationContext(),
-                mainRecyclerView, new RecyclerItemClickListener.OnItemClickListener() {
-            @Override
-            public void onItemClick(View view, int position) {
-                Movie movie = movieList.get(position);
-                Toast.makeText(getApplicationContext(), getString(R.string.pressed_item) + movie.getTitle(),
-                        Toast.LENGTH_LONG).show();
-            }
-
-            @Override
-            public void onLongItemClick(View view, int position) {
-                Movie movie = movieList.get(position);
-                Toast.makeText(getApplicationContext(), getString(R.string.long_click) + movie.getTitle(),
-                        Toast.LENGTH_LONG).show();
-            }
-
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
-            }
-        }));
     }
 
     private void setupRecyclerView(Adapter adapter) {
